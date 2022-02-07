d425425ca2501e4f8caf5eb92dcfd0bb9dc21728
felipelopezortega <flopezortega@ucol.mx>
2022-02-06 20:49:04 -0600

    function greetMe added and invoked

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ function logGreeting(fn){
 logGreeting(greet);
 // now the greet function is a parameter and we are invoking it
 //with the name of the "fn" argument
+
+//function expression
+let greetMe = function () {
+
+    console.log("Hello from the function expression");
+
+};
+
+greetMe ();
